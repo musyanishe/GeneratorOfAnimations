bac4e02611b619af4af8c7b30f5162b874a60f73
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-04-05 01:21:53 +0300

    added Animation.swift and screen at storyboard

---
+++ GeneratorOfAnimations/GeneratorOfAnimations/Animation.swift
@@ -0,0 +1,16 @@
+//
+//  Animation.swift
+//  GeneratorOfAnimations
+//
+//  Created by Евгения Шевцова on 05.04.2022.
+//
+
+import Foundation
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Float
+    let duration: Float
+    let delay: Float
+}
