aa33ef92bbc517389eb36c3a673a165852da5908
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-04-06 00:46:30 +0300

    edit ViewController, Model

--- GeneratorOfAnimations/GeneratorOfAnimations/Model/Animation.swift
+++ GeneratorOfAnimations/GeneratorOfAnimations/Model/Animation.swift
@@ -13,4 +13,25 @@ struct Animation {
     let force: Float
     let duration: Float
     let delay: Float
+    
+    var description: String {
+        """
+        preset: \(preset)
+        curve: \(curve)
+        force: \(String(format: "%.02f", force))
+        duration: \(String(format: "%.02f", duration))
+        delay: \(String(format: "%.02f", delay))
+        """
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(
+            preset: DataManager.shared.animation.randomElement()?.rawValue ?? "slideLeft",
+            curve: DataManager.shared.curve.randomElement()?.rawValue ?? "spring",
+            force: Float.random(in: 1...2),
+            duration: Float.random(in: 0...3),
+            delay: Float.random(in: 0.3...0.9)
+        )
+    }
+    
 }
--- GeneratorOfAnimations/GeneratorOfAnimations/View Controllers/ViewController.swift
+++ GeneratorOfAnimations/GeneratorOfAnimations/View Controllers/ViewController.swift
@@ -8,13 +8,30 @@
 import Spring
 
 class ViewController: UIViewController {
+   
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var springAnimationView: SpringView!
+    
+    private var animation = Animation.getRandomAnimation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        descriptionLabel.text = animation.description
+        
     }
 
     @IBAction func runAnimationButtonPressed(_ sender: SpringButton) {
+        descriptionLabel.text = animation.description
+        
+        springAnimationView.animation = animation.preset
+        springAnimationView.curve = animation.curve
+        springAnimationView.force = CGFloat(animation.force)
+        springAnimationView.duration = CGFloat(animation.duration)
+        springAnimationView.delay = CGFloat(animation.delay)
+        springAnimationView.animate()
+        
+        animation = Animation.getRandomAnimation()
+        sender.setTitle("Run: \(animation.description)", for: .normal)
     }
     
 }
