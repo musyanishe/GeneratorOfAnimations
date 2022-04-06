5a25293cb2aa13e471313405ab89e3e81e490a3d
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-04-06 03:06:28 +0300

    edit DataManager

--- GeneratorOfAnimations/GeneratorOfAnimations/Services/DataManager.swift
+++ GeneratorOfAnimations/GeneratorOfAnimations/Services/DataManager.swift
@@ -11,59 +11,8 @@ class DataManager {
     
     static let shared = DataManager()
     
-    let animation: [Spring.AnimationPreset] = [
-        .fadeIn,
-        .fadeInDown,
-        .fadeInLeft,
-        .fadeInRight,
-        .fadeOut,
-        .fadeInUp,
-        .fadeOutIn,
-        .fall,
-        .flash,
-        .flipX,
-        .flipY,
-        .morph,
-        .pop,
-        .shake,
-        .slideDown,
-        .slideLeft,
-        .slideUp,
-        .slideRight,
-        .squeeze,
-        .squeezeUp,
-        .squeezeDown,
-        .squeezeLeft,
-        .squeezeRight,
-        .swing,
-        .wobble,
-        .zoomIn,
-        .zoomOut
-    ]
-    
-    let curve: [Spring.AnimationCurve] = [
-        .easeIn,
-        .easeInBack,
-        .easeOut,
-        .easeInOut,
-        .easeInCirc,
-        .easeInCubic,
-        .easeInExpo,
-        .easeInQuad,
-        .easeInOutBack,
-        .easeInOutCirc,
-        .easeInOutCubic,
-        .easeInOutExpo,
-        .easeInQuart,
-        .easeInOutQuad,
-        .easeInOutSine,
-        .easeInOutQuint,
-        .easeOutCirc,
-        .easeOutCubic,
-        .linear,
-        .spring,
-        .easeOutSine
-    ]
-    
+    let animation = Spring.AnimationPreset.allCases
+    let curve = Spring.AnimationCurve.allCases
+
     private init() {}
 }
